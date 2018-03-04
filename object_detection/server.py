4deae69d0942766a5f3127601e5d271ea88cc7ad
Andrey Yashchak <andrey@yashchak.ru>
2018-03-04 08:26:34 +0300

    try to solve that problem

---
+++ object_detection/server.py
@@ -0,0 +1,72 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import json
+import time
+import glob
+
+from io import StringIO
+from PIL import Image
+
+import matplotlib.pyplot as plt
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+from multiprocessing.dummy import Pool as ThreadPool
+
+app = Flask(__name__)
+
+MAX_NUMBER_OF_BOXES = 10
+MINIMUM_CONFIDENCE = 0.6
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+MODEL_NAME = 'output_inference_graph'
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
+
+def detect_objects(image_path):
+    image = Image.open(image_path)
+    image_np = load_image_into_numpy_array(image)
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+
+    return sess.run([detection_boxes, detection_scores, detection_classes, num_detections], feed_dict={image_tensor: image_np_expanded})
+
+# Load model into memory
+print('Loading model...')
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+@app.route('/')
+def index():
+    print('detecting...')
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+            image_path = request.args.get('image')
+            print(image_path)
+            return image_path
+    # return jsonify(detect_objects(image_path))
+
+
+app.debug = True
+app.run(host='0.0.0.0')
